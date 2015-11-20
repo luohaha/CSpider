dc1a5fc4a090dad9b3c5979b9338f5922cee2ec6
Yixin Luo <18810541851@163.com>
2015-11-20 23:54:58 +0800

    完善中

--- core/Downloader/download.c
+++ core/Downloader/download.c
@@ -31,5 +31,56 @@ void curl_close_cb(uv_handle_t *handle) {
   free(context);
 }
 
+void destroy_curl_context(curl_context_t *context) {
+  uv_close((uv_handle_t*)&context->poll_handle, curl_close_cb);
+}
 
+/*
+  添加第num个url
+*/
+void add_download(const char* url, int num) {
+  char filename[50];
+  sprintf(filename, "%d.download", num);
 
+  File *file;
+
+  file = fopen(filename, "w");
+  if (file == NULL) {
+    fprintf(stderr, "error opening %s", filename);
+    return;
+  }
+
+  CURL *handle = curl_easy_init();
+  curl_easy_setopt(handle, CURLOPT_WRITEDATA, file);
+  curl_easy_setopt(handle, CURLOPT_URL, url);
+
+  curl_multi_add_handle(curl_handle, handle);
+  fprintf(stderr, "添加下载：%s -> %s\n", url, filename);
+}
+
+int main() {
+  loop = uv_default_loop();
+
+  if (argc <= 1) {
+    return 0;
+  }
+
+  if (curl_global_init(CURL_GLOBAL_ALL)) {
+    fprintf(stderr, "could not init curl\n");
+    return 1;
+  }
+
+  uv_timer_init(loop, &timeout);
+
+  curl_handle = curl_multi_init();
+  curl_multi_setopt(curl_handle, CURLMOPT_SOCKETFUNCTION, handle_socket);
+  curl_multi_setopt(curl_handle, CURLMOPT_TIMERFUNCION, start_timeout);
+
+  while(argc-- > 1) {
+    add_download(argv[argc], argc);
+  }
+
+  uv_run(loop, UV_RUN_DEFAULT);
+  curl_multi_cleanup(curl_handle);
+  return 0;
+}
