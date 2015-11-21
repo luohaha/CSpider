979dd976819cd7b3d865acbb9f00df270771fcbd
Yixin Luo <18810541851@163.com>
2015-11-21 13:31:47 +0800

    新建存放任务和数据的结构和队列

---
+++ core/CS.h
@@ -0,0 +1,45 @@
+#ifndef CS_H
+#define CS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<uv.h>
+#include<assert.h>
+
+/*
+  任务
+  存放要执行抓取的url
+*/
+typedef struct cs_task_struct {
+  char *url;//存放的url
+  int prior;//优先级1~10
+} cs_task_t;
+
+/*
+  任务队列
+*/
+typedef struct cs_task_queue_struct {
+  cs_task_t *task;
+  struct cs_task_queue_struct *next;
+  struct cs_task_queue_Struct *prev;
+} cs_task_queue;
+
+/*
+  原始数据
+  例如html，json等，直接返回的数据
+*/
+typedef struct cs_rawText_struct {
+  char *data;
+  char *type;//数据的类型，比如html，json
+} cs_rawText_t;
+
+/*
+  数据队列
+*/
+typedef struct cs_rawText_queue_struct {
+  cs_rawText_t *data;
+  struct cs_rawText_queue_struct *next;
+  struct cs_rawText_queue_struct *prev;
+}
+  
+#endif
