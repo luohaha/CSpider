b6d68b78df79c7beb60624a5c791b29a8836e125
Mike Taghavi <mitghi@me.com>
2015-12-27 15:31:10 +0100

    Indent

--- sample/main.c
+++ sample/main.c
@@ -6,7 +6,7 @@ void p(cspider_t *cspider, char *d, char *url, void *user_data) {
   int size = xpath(d, "//a/@href", get, 10);
 //  int size = regexAll("http:\\/\\/(.*?)\\.html", d, get, 3, REGEX_ALL);
 
-	joinall(url,get,size);
+  joinall(url,get,size);
   addUrls(cspider, get, size);
   saveStrings(cspider, (void**)get, size, LOCK);
   freeStrings(get, size);
@@ -22,7 +22,7 @@ void s(void *str, void *user_data) {
 int main() {
   cspider_t *spider = init_cspider();
   char *agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:42.0) Gecko/20100101 Firefox/42.0";
-	setlocale(LC_ALL,"");
+  setlocale(LC_ALL,"");
 
   cs_setopt_url(spider, "http://www.youku.com/v_olist/c_96_s_0_d_0_g_0_a_0_r_0_u_0_pt_0_av_0_ag_0_sg_0_mt_0_lg_0_q_0_pr_0_h_0_p_1.html");
 
