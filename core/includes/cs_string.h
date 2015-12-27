f61da1565d44da6111b614abf653b5458e115470
Nemo Yu <mzer0.yu@gmail.com>
2015-12-27 17:40:49 +0800

    Update cs_string.h

--- core/includes/cs_string.h
+++ core/includes/cs_string.h
@@ -2,21 +2,4 @@
 #define CS_STRING_H
 #endif
 
-#include "cd_page.h"
-
-int set_string(cs_page* p, char* context);
-
-inline int set_agent(cs_page* p, char* agent) {
-  p->file_type = FileTypeAgent;
-  return set_string(p, agent);
-}
-
-inline int set_proxy(cs_page* p, char* proxy) {
-  p->file_type = FileTypeProxy;
-  return set_string(p, proxy);
-}
-
-inline int set_agent(cs_page* p, char* cookie) {
-  p->file_type = FileTypeCookie;
-  return set_string(p, cookie);
-}
+#include "cs_page.h"
