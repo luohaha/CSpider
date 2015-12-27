8fbba65a99fb2f34a154b4a81ab52656e4308e68
Nemo Yu <mzer0.yu@gmail.com>
2015-12-27 17:32:43 +0800

    Update page_queue.h

--- core/includes/page_queue.h
+++ core/includes/page_queue.h
@@ -12,6 +12,6 @@ void destroy_page_queue(page_queue_id id);
 
 page_id alloc_page_from_queue(page_queue_id queue_id);
 
-free_page_from_queue(page_id pid);
+void free_page_from_queue(page_id pid);
 
 cs_page* get_page_from_id(page_id pid);
